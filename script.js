c17bc306ebe3fd401b849a6aeba154f6310853f0
comiratoalex-cmd <comiratoalex@gmail.com>
2025-11-18 23:48:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,57 +3,79 @@ const cols = [c1,c2,c3,c4,c5,c6];
 
 function updatePreview(){
   const colors = cols.map(c=>c.value);
-  const dir = direction.value;
-  const sp = speed.value;
-  const gl = glow.value;
-  const h  = height.value;
+  const dir = direction?.value || "90deg";
+  const sp = speed?.value || 6;
+  const gl = glow?.value || 10;
+  const h  = height?.value || 20;
 
   preview.style.height = h+'px';
   preview.style.animationDuration = sp+'s';
-  preview.style.boxShadow = `0 0 ${gl}px white`;
+  preview.style.boxShadow = `0 0 ${gl}px ${colors[2]}`;
   preview.style.background = `linear-gradient(${dir}, ${colors.join(', ')})`;
 }
 
-cols.concat([direction,speed,glow,height,preset]).forEach(el=>el.addEventListener('input',updatePreview));
+cols.forEach(el => el.addEventListener('input', updatePreview));
+
+if (preset) {
+  preset.addEventListener('change',()=>{
+    const sets={
+      aurora:['#00ffe7','#0095ff','#7f2bff','#ff4fd8','#ffbd39','#ff5e5e'],
+      pastel:['#ffbfd4','#ff9cf4','#c484ff','#8ac6ff','#a8fff1','#fff0b5'],
+      cyber:['#ff0062','#c800ff','#5200ff','#00c8ff','#00ffea','#ffea00'],
+      pride:['#ff0018','#ffa52c','#ffff41','#008018','#0000f9','#86007d'],
+      fog:['#6d7eff','#c95bff','#ff4fab','#ff7361','#ffd16b','#aafff4']
+    };
+
+    if(sets[preset.value]) {
+      [c1,c2,c3,c4,c5,c6].forEach((c,i)=>c.value=sets[preset.value][i]);
+    }
+    updatePreview();
+  });
+}
+
+if (direction) direction.addEventListener('input', updatePreview);
+if (speed) speed.addEventListener('input', updatePreview);
+if (glow) glow.addEventListener('input', updatePreview);
+if (height) height.addEventListener('input', updatePreview);
+
 updatePreview();
 
-preset.addEventListener('change',()=>{
-  const sets={
-    aurora:['#00ffe7','#0095ff','#7f2bff','#ff4fd8','#ffbd39','#ff5e5e'],
-    pastel:['#ffbfd4','#ff9cf4','#c484ff','#8ac6ff','#a8fff1','#fff0b5'],
-    cyber:['#ff0062','#c800ff','#5200ff','#00c8ff','#00ffea','#ffea00'],
-    pride:['#ff0018','#ffa52c','#ffff41','#008018','#0000f9','#86007d'],
-    fog:['#6d7eff','#c95bff','#ff4fab','#ff7361','#ffd16b','#aafff4']
-  };
-  if(sets[preset.value]) [c1,c2,c3,c4,c5,c6].forEach((c,i)=>c.value=sets[preset.value][i]);
-  updatePreview();
-});
-
-copyCSS.onclick=()=>{
-  navigator.clipboard.writeText(preview.style.background);
-  alert("CSS copied!");
+/* COPY CSS */
+copyCSS.onclick = () => {
+  const css = preview.style.background;
+  navigator.clipboard.writeText(css);
+  alert("CSS copied to clipboard!");
 };
 
-downloadPNG.onclick=()=>{
-  const canvas=document.createElement('canvas');
-  canvas.width=1920; canvas.height=parseInt(height.value);
-  const ctx=canvas.getContext('2d');
-  const g=ctx.createLinearGradient(0,0,canvas.width,0);
-  const c=cols.map(x=>x.value);
-  c.forEach((col,i)=>g.addColorStop(i/(c.length-1),col));
-  ctx.fillStyle=g;
-  ctx.fillRect(0,0,canvas.width,canvas.height);
-  const a=document.createElement('a');
-  a.download='neon.png';
-  a.href=canvas.toDataURL();
+/* PNG EXPORT */
+downloadPNG.onclick = () => {
+  const canvas = document.createElement('canvas');
+  canvas.width = 1920;
+  canvas.height = parseInt(height.value);
+  const ctx = canvas.getContext('2d');
+
+  const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+  const colors = cols.map(x => x.value);
+  colors.forEach((col, i) => gradient.addColorStop(i / (colors.length - 1), col));
+
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const a = document.createElement('a');
+  a.download = 'neon.png';
+  a.href = canvas.toDataURL();
   a.click();
 };
 
-function toggleFullscreen(){
-  if(!document.fullscreenElement) document.documentElement.requestFullscreen();
-  else document.exitFullscreen();
+/* FULLSCREEN */
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
 }
 
-downloadWEBM.onclick=()=>{
-  alert("WEBM export real será adicionado na próxima versão!");
+downloadWEBM.onclick = () => {
+  alert("WEBM animation export coming in next update!");
 };
